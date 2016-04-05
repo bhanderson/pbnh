8bf6308446a5f033d1dde2145fb52e529809fc68
Bryce Handerson <bryce.handerson@isilon.com>
2016-04-05 16:29:08 -0700

    get a requst set up

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["POST"])
 def hello():
+    print(request.data)
+    print(request.form)
+    print(request.stream)
+    print(request.files)
+    print(request.method)
+    print(request.values)
     return "Hello World!"
 
 if __name__ == "__main__":
-        app.run('0.0.0.0')
+        app.run('0.0.0.0', debug=True)

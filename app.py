8ae31c5dafd6fb5e10e10b9c62ea8fd93dc9d276
Bryce Handerson <bryce.handerson@isilon.com>
2016-04-06 13:59:57 -0700

    saving for idk

--- app.py
+++ app.py
@@ -1,17 +1,64 @@
 #!/bin/env/python
-from flask import Flask, request, Response, send_file, render_template, redirect
+from flask import Flask, request, send_file, render_template, redirect
 from pygments import highlight
 from pygments.formatters import HtmlFormatter
-from pygments.lexers import PythonLexer
-from pygments.lexers import guess_lexer
+from pygments.lexers import get_lexer_for_mimetype
+from werkzeug.datastructures import FileStorage
 import hashlib
 import io
 import magic
-from urllib.parse import urlparse
+import validators
 app = Flask(__name__)
+
+def filedata(fs):
+    try:
+        buf = fs.stream
+        if buf and isinstance(buf, io.BytesIO):
+            data = buf.read()
+            open('outfile', 'wb').write(data)
+            mime = magic.from_buffer(data, mime=True)
+            open('mime', 'wb').write(mime)
+            return hashlib.sha1(data).hexdigest(), mime
+    except IOError as e:
+        return 'caught exception in filedata' + str(e)
+    return 'File save error, your file is probably too big'
+
+def stringdata(inputstr):
+    if validators.url(inputstr): # the string is a url return a redirect
+        return redirect(inputstr, code=302)
+    encoded = inputstr.encode('utf-8')
+    return hashlib.sha1(encoded).hexdigest()
+
+def returnfile():
+    f = open('outfile', 'rb').read()
+    mime = open('mime', 'r').read()
+    if mime.split('/')[0] == 'text':
+        result = highlight(f, get_lexer_for_mimetype(mime),
+                           HtmlFormatter(linenos=True, full=True,
+                                         style='colorful'))
+        return render_template('paste.html', paste=result)
+    binstream = io.BytesIO(f)
+    return send_file(binstream, mimetype=mime)
 
 @app.route("/", methods=["GET", "POST"])
 def hello():
+    if request.method == 'POST':
+        inputstr = request.form.get('c')
+        if inputstr and isinstance(inputstr, str): # we didn't get a file we got a string
+            return stringdata(inputstr)
+
+        files = request.files.get('c')
+        if files and isinstance(files, FileStorage): # we got a file
+            return filedata(files)
+    else:
+        print("get")
+        return returnfile()
+    print(request.form)
+    print(request.files)
+    print('fell through')
+    return 'fell through'
+
+"""
     if request.method == 'POST':
         fs = request.files.get('c', 'content')
         if fs and not type(fs) is str:
@@ -47,6 +94,6 @@ def hello():
             pass
         else:
             return send_file(binstream, attachment_filename=filename, mimetype=mime)
-
+"""
 if __name__ == "__main__":
-    app.run('0.0.0.0', debug=True)
+    app.run('0.0.0.0', port=5001, debug=True)

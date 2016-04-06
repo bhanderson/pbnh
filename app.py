dddc1577061f22bce518a1f77e217c0c40fb28ae
Bryce Handerson <bryce.handerson@isilon.com>
2016-04-05 21:09:52 -0700

    return png and install magic

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
-from flask import Flask, request
+from flask import Flask, request, Response, send_file
+import io
 app = Flask(__name__)
 
-@app.route("/", methods=["POST"])
+@app.route("/", methods=["GET", "POST"])
 def hello():
-    print(request.files)
-    print(request.headers)
-    print(request.url)
-    print(request.url_root)
-    fs = request.files.get('c', 'content')
-    if fs:
-        print(fs.read())
-        print(fs.stream)
-    return "Hello World!"
+    if request.method == 'POST':
+        print(request.files)
+        print(request.headers)
+        print(request.url)
+        print(request.url_root)
+        fs = request.files.get('c', 'content')
+        if fs:
+            #print(fs.stream)
+            open('filename', 'wb').write(fs.stream.getbuffer())
+        return "Hello World!"
+    else:
+        filename = 'filename'
+        return send_file(io.BytesIO(io.open(filename, 'rb').read()),
+                attachment_filename=filename,mimetype='image/png')
 
 if __name__ == "__main__":
-        app.run('0.0.0.0', debug=True)
+    app.run('0.0.0.0', debug=True)

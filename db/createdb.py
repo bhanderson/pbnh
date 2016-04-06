c65bd4e3e0e599d19be655170ff3cfda746fc162
Ethan Madden <emadden@isilon.com>
2016-04-06 15:26:40 -0700

    abstract out connection string generation

--- db/createdb.py
+++ db/createdb.py
@@ -1,32 +1,8 @@
 from sqlalchemy import create_engine
-from sqlalchemy_utils import create_database, database_exists
+from sqlalchemy_utils import create_database
 
 import models
-
-class DBConnect():
-    """create db connection string"""
-    def __init__(self, dialect=None, driver=None, username=None, password=None,
-                 host=None, port=None, dbname=None):
-        self._connect = dialect
-        if driver:
-            self._connect += '+' + driver
-        self._connect += '://'
-        if username:
-            self._connect += username
-            if password:
-                self._connect += ':' + password
-            self._connnect +='@'
-        if host:
-            self._connect += host
-            if port:
-                self._connect += ':' + port
-        if dbname:
-            self._connect += '/' + dbname
-
-    @property
-    def connect(self):
-        """Connection string read-only property"""
-        return self._connect
+from connect import DBConnect
 
 def main():
     connection = DBConnect(dialect='sqlite', dbname='test.db').connect

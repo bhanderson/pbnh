5f1bed5d66f42991db951f983a702af7575e890d
jetpacktuxedo <crazeh.monkeh@gmail.com>
2016-04-07 20:20:32 -0700

    added unit tests for psql

---
+++ tests/paste_psql_test.py
@@ -0,0 +1,121 @@
+from datetime import datetime
+import os
+import unittest
+
+from sqlalchemy_utils import drop_database
+
+from db.createdb import CreateDB
+from db import paste
+from db.connect import DBConnect
+
+dialect = 'postgresql'
+dbname = 'testdb'
+
+class TestPaster(unittest.TestCase):
+    def setUp(self):
+        self.newdb = CreateDB(dialect=dialect, dbname=dbname)
+        self.newdb.create()
+
+    def tearDown(self):
+        drop_database(str(DBConnect(dialect=dialect, dbname=dbname)))
+
+    def test_create_new(self):
+        with paste.Paster(dialect, dbname) as p:
+            created = p.create(b'This is a test paste')
+        self.assertEqual(
+                created,
+                {
+                    'id': 1,
+                    'hashid': 'f872a542a8289d2273f6cb455198e06126f4ec30'
+                    }
+                )
+
+    def test_create_dupe(self):
+        with paste.Paster(dialect, dbname) as p:
+            created = p.create(b'This is a test paste')
+            created = p.create(b'This is a test paste')
+        self.assertEqual(
+                created,
+                {
+                    'id': 'HASH COLLISION',
+                    'hashid': 'f872a542a8289d2273f6cb455198e06126f4ec30'
+                    }
+                )
+
+    def test_query_id(self):
+        timestamp = datetime.now()
+        with paste.Paster(dialect, dbname) as p:
+            p.create(b'This is a test paste', timestamp=timestamp)
+            lookup = p.query(id=1)
+        self.assertEqual(
+                lookup,
+                {
+                    'id': 1,
+                    'hashid': 'f872a542a8289d2273f6cb455198e06126f4ec30',
+                    'ip': None,
+                    'mac': None,
+                    'mime': 'text/plain',
+                    'sunset': None,
+                    'timestamp': timestamp,
+                    'data': b'This is a test paste'
+                    }
+                )
+
+    def test_query_hash(self):
+        timestamp = datetime.now()
+        with paste.Paster(dialect, dbname) as p:
+            p.create(b'This is a test paste', timestamp=timestamp)
+            lookup = p.query(hashid='f872a542a8289d2273f6cb455198e06126f4ec30')
+        self.assertEqual(
+                lookup,
+                {
+                    'id': 1,
+                    'hashid': 'f872a542a8289d2273f6cb455198e06126f4ec30',
+                    'ip': None,
+                    'mac': None,
+                    'mime': 'text/plain',
+                    'sunset': None,
+                    'timestamp': timestamp,
+                    'data': b'This is a test paste'
+                    }
+                )
+
+    def test_query_none(self):
+        with paste.Paster(dialect, dbname) as p:
+            p.create(b'This is a test paste')
+            lookup = p.query()
+        self.assertEqual(lookup, None)
+
+    def test_delete_id(self):
+        with paste.Paster(dialect, dbname) as p:
+            p.create(b'This is a test paste')
+            p.delete(id=1)
+            lookup = p.query(id=1)
+        self.assertEqual(lookup, None)
+
+    def test_delete_hash(self):
+        with paste.Paster(dialect, dbname) as p:
+            p.create(b'This is a test paste')
+            p.delete(hashid='f872a542a8289d2273f6cb455198e06126f4ec30')
+            lookup = p.query(id=1)
+        self.assertEqual(lookup, None)
+
+    def test_delete_none(self):
+        timestamp = datetime.now()
+        with paste.Paster(dialect, dbname) as p:
+            p.create(b'This is a test paste', timestamp=timestamp)
+            p.delete()
+            lookup = p.query(id=1)
+        self.assertEqual(
+                lookup,
+                {
+                    'id': 1,
+                    'hashid': 'f872a542a8289d2273f6cb455198e06126f4ec30',
+                    'ip': None,
+                    'mac': None,
+                    'mime': 'text/plain',
+                    'sunset': None,
+                    'timestamp': timestamp,
+                    'data': b'This is a test paste'
+                    }
+                )

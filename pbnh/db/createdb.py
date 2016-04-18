60fe00f809c2fc4c9e514ae79eca24af2b62a940
jetpacktuxedo <crazeh.monkeh@gmail.com>
2016-04-17 17:46:17 -0700

    better arg handling

--- pbnh/db/createdb.py
+++ pbnh/db/createdb.py
@@ -7,23 +7,27 @@ from pbnh.db import models
 from pbnh.db.connect import DBConnect
 
 class CreateDB():
-    def __init__(self, dialect=None, dbname=None):
+    def __init__(self, dialect=None, driver=None, username=None, password=None,
+                 host=None, port=None, dbname=None):
+        """Grab connection information to pass to DBConnect"""
         self.dialect = dialect or 'sqlite'
         self.dbname = dbname or 'test'
+        self.driver = driver
+        self.username = username
+        self.password = password
+        self.host = host
+        self.port = port
 
     def create(self):
-        if self.dialect == 'sqlite':
-            connection = DBConnect(
-                    dialect='sqlite',
-                    dbname=self.dbname
-                    )
-        elif self.dialect == 'postgresql':
-            connection = DBConnect(
-                    dialect='postgresql',
-                    dbname=self.dbname
-                    )
-        else:
-            return 'No database could be created'
+        connection = DBConnect(
+                dialect=self.dialect,
+                driver=self.driver,
+                username=self.username,
+                password=self.password,
+                host=self.host,
+                port=self.port,
+                dbname=self.dbname
+                )
         create_database(str(connection))
         engine = create_engine(str(connection))
         models.Base.metadata.create_all(engine)
@@ -34,10 +38,21 @@ def main():
     parser = argparse.ArgumentParser(description='Initialize a paste db')
     parser.add_argument('-t', '--type', default=config.get('dialect'),
                         help='sqlite or postgresql')
-    parser.add_argument('-d', '--dbname', default=config.get('dbname'),
+    parser.add_argument('-n', '--dbname', default=config.get('dbname'),
                         help='name of the database to be created')
+    parser.add_argument('-d', '--driver', default=config.get('driver'),
+                        help='database driver for sqlalchemy to use')
+    parser.add_argument('-u', '--username', default=config.get('host'),
+                        help='username to use for the database connection')
+    parser.add_argument('-p', '--password', default=config.get('password'),
+                        help='password to use for the database connection')
+    parser.add_argument('-h', '--host', default=config.get('port'),
+                        help='host of the database')
+    parser.add_argument('-P', '--port', default=config.get('username'),
+                        help='port the database listens on')
     args = parser.parse_args()
-    newdb = CreateDB(args.type, args.dbname)
+    newdb = CreateDB(args.type, args.dbname, args.driver, args.username, args.password,
+                     args.host, args.port)
     print(newdb.create())
 
 if __name__ == "__main__":

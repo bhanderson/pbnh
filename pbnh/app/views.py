e529f3d42675eb3a33183817ee44491bda35e853
Bryce Handerson <bryce.handerson@isilon.com>
2016-04-22 13:32:23 -0700

    js templating still need to remove

--- pbnh/app/views.py
+++ pbnh/app/views.py
@@ -1,8 +1,8 @@
 import io
 import re
-
-from flask import request, send_file, render_template, Response, redirect
 import pygments
+
+from flask import request, send_file, render_template, Response
 from pygments import highlight, util, formatters
 from pygments.lexers import get_lexer_for_mimetype, get_lexer_for_filename
 from sqlalchemy import exc
@@ -25,8 +25,10 @@ def hello():
         addr = request.remote_addr
         # First check c for paste content
         inputstr = request.form.get('c')
-        sunset = int(request.form.get('sunset'))
-        return 'hi'
+        sunsetstr = request.form.get('sunset')
+        sunset = None
+        if sunsetstr:
+            sunset = int(sunsetstr)
         if sunset and isinstance(sunset, int):
             sunset = datetime.now(timezone.utc) + timedelta(seconds=sunset)
         if inputstr and isinstance(inputstr, str): # we didn't get a file we got a string
@@ -63,6 +65,7 @@ def view_paste(paste_id, filetype=None, hashid=False):
             if mime == 'redirect':
                 return redirect(data)
             if mime[:4] == 'text':
+                print(mime[5:])
                 if not filetype:
                     lexer = get_lexer_for_mimetype(mime)
                 else:
@@ -74,7 +77,8 @@ def view_paste(paste_id, filetype=None, hashid=False):
                                  formatters.HtmlFormatter(style='colorful',
                                                           full=True,
                                                           linenos=True))
-                return render_template('paste.html', paste=html)
+                #return render_template('paste.html', paste=html)
+                return render_template('paste.html', paste=data.decode('utf-8'), mime=mime[5:])
             data = io.BytesIO(query.get('data'))
             return send_file(data, mimetype=mime)
         if not hashid:

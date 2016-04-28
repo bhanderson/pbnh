f7c5909cac76c622a2c98c802b9634b895ba4ad8
Bryce Handerson <bryce.handerson@isilon.com>
2016-04-28 12:34:25 -0700

    added more tests

--- pbnh/tests/paste_curl_test.py
+++ pbnh/tests/paste_curl_test.py
@@ -6,6 +6,7 @@ import os
 from pbnh import conf
 from pbnh import app
 from pbnh.db.createdb import CreateDB
+from io import BytesIO
 
 
 DEFAULTS = {
@@ -49,7 +50,16 @@ class TestPost(unittest.TestCase):
         response = self.app.get('/1')
         self.assertEqual(response.status_code, 404)
 
-    def test_paste(self):
+    def test_paste_string_c(self):
+        response = self.app.post('/', data={'c': 'abc'})
+        j = json.loads(response.data.decode('utf-8'))
+        self.failUnlessEqual(j.get('hashid'),
+                'a9993e364706816aba3e25717850c26c9cd0d89d')
+        self.assertEqual(response.status_code, 201)
+        response = self.app.get('/1')
+        self.assertEqual(response.status_code, 200)
+
+    def test_paste_string_content(self):
         response = self.app.post('/', data={'content': 'abc'})
         j = json.loads(response.data.decode('utf-8'))
         self.failUnlessEqual(j.get('hashid'),
@@ -57,3 +67,37 @@ class TestPost(unittest.TestCase):
         self.assertEqual(response.status_code, 201)
         response = self.app.get('/1')
         self.assertEqual(response.status_code, 200)
+
+    def test_redirect(self):
+        response = self.app.post('/', data={'r': 'http://www.google.com'})
+        j = json.loads(response.data.decode('utf-8'))
+        self.failUnlessEqual(j.get('hashid'),
+                '738ddf35b3a85a7a6ba7b232bd3d5f1e4d284ad1')
+        self.assertEqual(response.status_code, 201)
+
+    def test_paste_file_c(self):
+        response = self.app.post('/', data={'c': (BytesIO(b"contents"),
+            'test')})
+        self.assertEqual(response.status_code, 201)
+        j = json.loads(response.data.decode('utf-8'))
+        self.failUnlessEqual(j.get('hashid'),
+                '4a756ca07e9487f482465a99e8286abc86ba4dc7')
+
+    def test_paste_file_c(self):
+        response = self.app.post('/', data={'content': (BytesIO(b"contents"),
+            'test')})
+        self.assertEqual(response.status_code, 201)
+        j = json.loads(response.data.decode('utf-8'))
+        self.failUnlessEqual(j.get('hashid'),
+                '4a756ca07e9487f482465a99e8286abc86ba4dc7')
+
+    def test_paste_extension(self):
+        response = self.app.post('/', data={'content': 'abc'})
+        response = self.app.get('/1.txt')
+        self.assertEqual(response.status_code, 200)
+
+    def test_paste_highlight(self):
+        response = self.app.post('/', data={'content': 'abc'})
+        response = self.app.get('/1/txt')
+        self.assertEqual(response.status_code, 200)
+

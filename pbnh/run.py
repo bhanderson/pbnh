942e2446db1307d688d82129257d1d62dccbe268
Ethan Madden <emadden@isilon.com>
2018-10-17 17:13:38 -0700

    Smashed code into docker-shaped box

--- pbnh/run.py
+++ pbnh/run.py
@@ -1,13 +1,1 @@
 from pbnh.app import app
-import yaml
-
-config = {}
-with open('secrets.yml') as f:
-    config = yaml.load(f)
-
-app.config['CONFIG'] = config
-if config:
-    app.run(config.get('server').get('bind_ip'),
-    port=config.get('server').get('bind_port'))
-else:
-    exit(1)
